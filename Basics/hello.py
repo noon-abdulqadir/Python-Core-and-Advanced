d89442a0132f002f9d8a0f589f598be6a5bb2bd4
Noon MIA Fatah Elrahman <46266930+nyXiNsane@users.noreply.github.com>
2020-04-24 04:37:16 +0200

    .

--- Basics/hello.py
+++ Basics/hello.py
@@ -1,6 +1,10 @@
+import sys
+print(sys.executable)
+print(sys.version)
+
 """
 Author is Noon
 Created during the corona pandemic
 """
-#This is my python program
+# This is my python program
 print("Python is easy.")
--- Inheretence/BMW.py
+++ Inheretence/BMW.py
@@ -1,38 +1,41 @@
 class BMW:
-    
-    def __init__(self,make,model,year):
-        self.make=make
-        self.model=model
-        self.year=year
-    
+
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
     def start(self):
         print("Starting the car.")
-    
+
     def stop(self):
         print("Stopping the car.")
-    
-class ThreeSeries(BMW):#inherit
-    
-    def __init__(self,CruiseControlEnabled,make,model,year):
+
+
+class ThreeSeries(BMW):  # inherit
+
+    def __init__(self, CruiseControlEnabled, make, model, year):  # or def __init__(self,CruiseControlEnabled,*args,**kwargs)
         #BMW.__init__(self, make, model, year)
-        super().__init__(make, model, year)
-        self.CruiseControlEnabled=CruiseControlEnabled
-    
+        super().__init__(make, model, year)  # or super(ThreeSeries, self).__init__(*args,**kwargs)
+        self.CruiseControlEnabled = CruiseControlEnabled
+
     def display(self):
         print(self.CruiseControlEnabled)
-    
+
     def start(self):
         super().start()
         print("Button Start.")
-    
-class FiveSeries(BMW):#inherit
-    
-    def __init__(self,ParkingAssistEnabled,make,model,year):
+
+
+class FiveSeries(BMW):  # inherit
+
+    def __init__(self, ParkingAssistEnabled, make, model, year):
         BMW.__init__(self, make, model, year)
         #super().__init__(make, model, year)
-        self.ParkingAssistEnabled=ParkingAssistEnabled
+        self.ParkingAssistEnabled = ParkingAssistEnabled
 
-threeSeries = ThreeSeries(True,"BMW","328i",2018)
+
+threeSeries = ThreeSeries(True, "BMW", "328i", 2018)
 print(threeSeries.CruiseControlEnabled)
 print(threeSeries.make)
 print(threeSeries.model)
@@ -42,4 +45,4 @@ threeSeries.start()
 threeSeries.stop()
 threeSeries.display()
 
-fiveSeries = FiveSeries(True,"BMW","328i",2018)
+fiveSeries = FiveSeries(True, "BMW", "328i", 2018)
